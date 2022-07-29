32c7a31e7c2881a0cdb9de5bbda2e25831fb8ff9
axtellion <slark19735@gmail.com>
2022-07-29 03:31:23 +0300

    fix bugs

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,43 +8,42 @@ import TrackVisibility from 'react-on-screen';
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const toRotate = ['Web Developer', 'Web Designer', 'UI/UX Designer'];
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const period = 2000;
 
   useEffect(() => {
     let ticker = setInterval(() => {
+      const tick = () => {
+        const toRotate = ['Web Developer', 'Web Designer', 'UI/UX Designer'];
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updateText = isDeleting
+          ? fullText.substring(0, text.length - 1)
+          : fullText.substring(0, text.length + 1);
+
+        setText(updateText);
+
+        if (isDeleting) {
+          setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updateText === fullText) {
+          setIsDeleting(true);
+          setDelta(period);
+        } else if (isDeleting && updateText === '') {
+          setIsDeleting(false);
+          setLoopNum(loopNum + 1);
+          setDelta(500);
+        }
+      };
       tick();
     }, delta);
 
     return () => {
       clearInterval(ticker);
     };
-  }, [text]);
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updateText = isDeleting
-      ? fullText.substring(0, text.length - 1)
-      : fullText.substring(0, text.length + 1);
-
-    setText(updateText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updateText === fullText) {
-      setIsDeleting(true);
-      setDelta(period);
-    } else if (isDeleting && updateText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setDelta(500);
-    }
-  };
+  }, [delta, isDeleting, loopNum, text]);
 
   return (
     <section className="banner" id="home">
